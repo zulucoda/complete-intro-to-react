0a252c530620bf56e5a3e5c33e1b42788f39e87d
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2016-10-09 05:55:29 +0200

    FEM step 4

---
+++ js/MyTitle.js
@@ -0,0 +1,18 @@
+/**
+ * Created by muzikayise on 2016/10/09.
+ */
+var React = require('react')
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title))
+    )
+  }
+})
+
+module.exports = MyTitle
